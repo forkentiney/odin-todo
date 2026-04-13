5f413cfb088e5b880dbad719fec105fd5fac94f6
Forkentiney <jseverud@proton.me>
2026-04-13 10:31:06 -0500

    Logic for controller started.
    Honestly, this pretty much completes the todo project barring the DOM
    stuff which is very time consuming. Since I am working with a very
    limited schedule, I may chalk this up to "good enough" and come back to
    it later.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const projectList = document.querySelector("#project-list");
+const projectDetails = document.querySelector("#project-details");
+
 const categories = [];
 
 function createCategory(title, details) {
@@ -20,3 +23,32 @@ function createCategory(title, details) {
 };
 
 createCategory("Default", "For all your general needs");
+
+function clearContent(content) {
+	while(content.childElementCount >= 1) {
+		content.removeChild(content.firstChild);
+	};
+};
+
+function controller() {
+	clearContent(projectList);
+	categories.forEach((category) => {
+		const projectTitle = document.createElement("div");
+		projectTitle.textContent = category.title;
+
+		projectTitle.addEventListener("click", function() {
+			clearContent(projectDetails);
+			categoryTitle = document.createElement("h1");
+			categoryTitle.textContent = category.title;
+			projectDetails.appendChild(categoryTitle);
+			category.todo.forEach((item) => {
+				const todoTitle = document.createElement("div");
+				todoTitle.textContent = item.title;
+
+				projectDetails.appendChild(todoTitle);
+			});
+		});
+
+		projectList.appendChild(projectTitle);
+	});
+}
