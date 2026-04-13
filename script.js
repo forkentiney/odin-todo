4021a1f9a92f309a9fed03527e7e5dc0e8de4630
Forkentiney <jseverud@proton.me>
2026-04-13 08:58:21 -0500

    Restructured functions
    Functionality is the same, but now it is less verbose and categories get
    put into an array as they are created. This will make it easy to place
    everything on the window

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-function category(title, details) {
-  const category = title;
-  const description = details;
-  const todo = [];
-  const done = [];
+const categories = [];
 
-	const createItem = (name) => {
-		const item = {
-			title: name,
-			active: 1,
-			priority: "normal",
-		};
-		todo.push(item);
+function createCategory(title, details) {
+	const category = {
+		title: title,
+		description: details,
+		todo: [],
+		done: [],
+		createItem: (name) => {
+			const item = {
+				title: name,
+				active: 1,
+				priority: "normal",
+			};
+			category.todo.push(item);
+		},
 	};
-
-  return { category, description, todo, done, createItem };
+	categories.push(category);
+	return { category };
 };
 
-const defaultCategory = category("Default", "For all your general needs");
+createCategory("Default", "For all your general needs");
